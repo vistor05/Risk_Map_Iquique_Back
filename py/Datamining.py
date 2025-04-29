b746bccf8b31ba4e5b63b2ca045d4b9e0320165f
Kris <christianha788@gmail.com>
2025-04-28 22:45:42 -0400

    refactor-Damining: code optimization

--- py/Datamining.py
+++ py/Datamining.py
@@ -1,173 +1,98 @@
-##Importacion de librerias
+## Importación de librerías
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from datetime import datetime
-from datetime import time
 import folium
 import statistics
-from folium.plugins import HeatMap
-##Carga y asignacion del dataset
+
+def crear_mapa(X, pesos, latitudes, longitudes, nombre_archivo, tooltip_centro, tooltip_zona):
+    """Función que entrena KMeans y genera el mapa"""
+    clusters = 2
+    kmeans = KMeans(n_clusters=clusters, random_state=42)
+    kmeans.fit(X, sample_weight=pesos)
+
+    media_lat = statistics.mean(latitudes)
+    media_lon = statistics.mean(longitudes)
+
+    mapa = folium.Map(location=[media_lat, media_lon], zoom_start=13)
+
+    # Colores y radios para los círculos
+    radios_colores = [
+        (750, "#0078FF", tooltip_zona),
+        (600, "#00FF3E", tooltip_zona),
+        (300, "#FFF700", tooltip_centro),
+        (180, "#FF9300", tooltip_centro),
+        (100, "#FF0000", tooltip_centro),
+    ]
+
+    for center in kmeans.cluster_centers_:
+        for radio, color, tooltip in radios_colores:
+            folium.Circle(
+                location=[center[0], center[1]],
+                tooltip=tooltip,
+                radius=radio,
+                fill=True,
+                color=color
+            ).add_to(mapa)
+
+    mapa.save(f"./mapas/{nombre_archivo}")
 
 def execute_scripts():
-    url='https://raw.githubusercontent.com/Chriiistian/ProyectPART/Pagina_Web(J)/CSV/Data%20frame%20final.csv'
-    dbf=pd.read_csv(url)
-    dbf=dbf.drop(['Unnamed: 0'], axis=1)
+    """Función principal"""
+    # Carga de datos
+    url = 'https://raw.githubusercontent.com/Chriiistian/ProyectPART/Pagina_Web(J)/CSV/Data%20frame%20final.csv'
+    dbf = pd.read_csv(url)
+    dbf = dbf.drop(columns=['Unnamed: 0'])
 
-    ##definicion de variables
-    Lat= dbf['LAT']
-    Lon = dbf['LNG']
-    fc = dbf['Fallecidos']
-    gv = dbf['Graves']
-    mgv = dbf['Menos_Grav']
-    lv= dbf['Leves']
-    ct = dbf['Count_acc']
-    ##creacion y asignacion del algoritmo
-    X = []
-    for i in range(len(dbf['LAT'])):
-        X.append(Lat[i])
-        X.append(Lon[i])
+    # Variables de interés
+    latitudes = dbf['LAT']
+    longitudes = dbf['LNG']
+    X = np.column_stack((latitudes, longitudes))  # Más eficiente que tu anterior bucle
 
-    X = np.array(X)
+    # Definición de los mapas a generar
+    mapas_info = [
+        {
+            "pesos": dbf['Count_acc'],
+            "nombre": "Map1.html",
+            "tooltip_centro": "Centro con más frecuencia de accidentes",
+            "tooltip_zona": "Zona con más frecuencia de accidentes"
+        },
+        {
+            "pesos": dbf['Fallecidos'],
+            "nombre": "Map2.html",
+            "tooltip_centro": "Centro de zona con más frecuencia de fallecidos",
+            "tooltip_zona": "Zona con más frecuencia de fallecidos"
+        },
+        {
+            "pesos": dbf['Graves'],
+            "nombre": "Map3.html",
+            "tooltip_centro": "Centro de zona con más frecuencia de lesionados graves",
+            "tooltip_zona": "Zona con más frecuencia de lesionados graves"
+        },
+        {
+            "pesos": dbf['Menos_Grav'],
+            "nombre": "Map4.html",
+            "tooltip_centro": "Centro de zona con más frecuencia de lesionados menos graves",
+            "tooltip_zona": "Zona con más frecuencia de lesionados menos graves"
+        },
+        {
+            "pesos": dbf['Leves'],
+            "nombre": "Map5.html",
+            "tooltip_centro": "Centro de zona con más frecuencia de lesionados leves",
+            "tooltip_zona": "Zona con más frecuencia de lesionados leves"
+        },
+    ]
 
-    X = X.reshape(-1, 2,)
+    # Generar cada mapa
+    for mapa in mapas_info:
+        crear_mapa(
+            X=X,
+            pesos=mapa["pesos"],
+            latitudes=latitudes,
+            longitudes=longitudes,
+            nombre_archivo=mapa["nombre"],
+            tooltip_centro=mapa["tooltip_centro"],
+            tooltip_zona=mapa["tooltip_zona"]
+        )
 
-    clusters = 2
-    KMean = KMeans(n_clusters=clusters)
-    KMean_g = KMean.fit_predict(X)
-    KMean.fit(X, sample_weight=ct)
-
-    KMean.cluster_centers_
-
-    ## MAPA 1 Cantidad de Accidentes
-
-    mediaLong = statistics.mean(Lon)
-    mediaLat = statistics.mean(Lat)
-
-    mapa = folium.Map(location=[mediaLat, mediaLong], zoom_start = 13)
-
-
-    tooltip = 'Centro con mas frecuencia de accidentes'  
-    tooltip1 = 'Zona con mas frecuencia de accidentes'   
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-
-    mapa.save("./mapas/Map1.html")
-
-    ##MAPA 2 CAntidad de Fallecidos
-
-    clusters = 2
-    KMean = KMeans(n_clusters=clusters)
-    KMean_g = KMean.fit_predict(X)
-    KMean.fit(X, sample_weight=fc)
-
-    mediaLong = statistics.mean(Lon)
-    mediaLat = statistics.mean(Lat)
-
-    mapa = folium.Map(location=[mediaLat, mediaLong], zoom_start = 13)
-
-    tooltip = 'Centro de zona con mas frecuencia de fallecidos'
-    tooltip1 = 'Zona con mas frecuencia de fallecidos' 
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-
-    mapa.save("./mapas/Map2.html")
-
-    ##MAPA 3 CAntidad de Lesionados Grave
-
-    clusters = 2
-    KMean = KMeans(n_clusters=clusters)
-    KMean_g = KMean.fit_predict(X)
-    KMean.fit(X, sample_weight=gv)
-
-    mediaLong = statistics.mean(Lon)
-    mediaLat = statistics.mean(Lat)
-
-    mapa = folium.Map(location=[mediaLat, mediaLong], zoom_start = 13)
-
-    tooltip = 'Centro de zona con mas frecuencia de lesionados graves'
-    tooltip1 = 'Zona con mas frecuencia de lesionados graves'
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-
-    mapa.save("./mapas/Map3.html")
-
-    ##MAPA 4 CAntidad de Lesionados Menos Graves
-
-    clusters = 2
-    KMean = KMeans(n_clusters=clusters)
-    KMean_g = KMean.fit_predict(X)
-    KMean.fit(X, sample_weight=mgv)
-
-    mediaLong = statistics.mean(Lon)
-    mediaLat = statistics.mean(Lat)
-
-    mapa = folium.Map(location=[mediaLat, mediaLong], zoom_start = 13)
-
-    tooltip = 'Centro de zona con mas frecuencia de lesionados menos graves'
-    tooltip1 = 'Zona con mas frecuencia de lesionados menos graves'
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-
-    mapa.save("./mapas/Map4.html")
-
-    ##MAPA 5 CAntidad de Lesionados Leves
-
-    clusters = 2
-    KMean = KMeans(n_clusters=clusters)
-    KMean_g = KMean.fit_predict(X)
-    KMean.fit(X, sample_weight=lv)
-
-    mediaLong = statistics.mean(Lon)
-    mediaLat = statistics.mean(Lat)
-
-    mapa = folium.Map(location=[mediaLat, mediaLong], zoom_start = 13)
-
-
-    tooltip = 'Centro de zona con mas frecuencia de lesionados leves'
-    tooltip1 = 'Zona con mas frecuencia de lesionados leves'
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 750, fill = True,color = "#0078FF" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip1, radius= 600, fill = True,color = "#00FF3E" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 300, fill = True,color = "#FFF700" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 180, fill = True,color = "#FF9300" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[0][0], KMean.cluster_centers_[0][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-    folium.Circle([KMean.cluster_centers_[1][0], KMean.cluster_centers_[1][1]], tooltip = tooltip, radius= 100, fill = True,color = "#FF0000" ).add_to(mapa)
-
-    mapa.save("./mapas/Map5.html")
